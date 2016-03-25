21c16810c59ac8221e0258565ac09d5494f3f6e5
Mickey <develop.agility@gmail.com>
2016-03-24 19:17:37 -0600

    improved erra handling

--- express.js
+++ express.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var path = require('path');
+var logger = require('morgan');
 var bodyParser = require('body-parser');
-var index = require('./routes/index');
 var home = require('./routes/home');
+var dbRouter = require('./routes/dbRouter');
 var app = express();
 
+app.use(logger('dev'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'/public')));
 app.use('/', home);
-app.use('/api/v1', index);
+app.use('/api/v1', dbRouter);
 
 app.use((err, req, res, next) =>{
   res.status(err.status || 500);
